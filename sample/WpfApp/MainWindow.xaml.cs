149c351f3826038db565195c2eea74383378a124
김동규 <hapoom-s@daum.net>
2017-10-06 20:32:09 +0900

    20171006

--- sample/WpfApp/MainWindow.xaml.cs
+++ sample/WpfApp/MainWindow.xaml.cs
@@ -1,31 +1,12 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media;
-using Hapoom.Windows;
+﻿using Hapoom.Windows;
 
 namespace WpfApp
 {
     public partial class MainWindow : WindowBase
     {
-        MainViewModoel _vm = new MainViewModoel();
-
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = _vm;
-            _vm.Title = "하아푸움 hapoom-s.tistory.com";
-        }
-
-        private void Button_Click1(object sender, RoutedEventArgs e)
-        {
-            ChangeBlackTheme();
-        }
-
-        private void Button_Click2(object sender, RoutedEventArgs e)
-        {
-            ChangeWhiteTheme();
         }
     }
 }
